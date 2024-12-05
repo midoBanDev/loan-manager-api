36949a4e298e949318f4c52d644f66bd19e74771
midoBanDev <amd0223@gmail.com>
2024-12-05 22:45:54 +0900

    gt-platform first commit

---
+++ src/main/java/com/gt/GtPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtPlatformApplication.class, args);
+	}
+
+}
