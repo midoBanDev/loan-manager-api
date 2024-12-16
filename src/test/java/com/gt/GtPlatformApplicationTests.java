92c466ba3f757c6ddc09fb2980c64aa429661273
midoBanDev <amd0223@gmail.com>
2024-12-16 22:17:53 +0900

    test main 메서드 어노테이션 추가

--- src/test/java/com/gt/GtPlatformApplicationTests.java
+++ src/test/java/com/gt/GtPlatformApplicationTests.java
@@ -7,10 +7,12 @@ import org.springframework.context.ApplicationContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@SpringBootTest
 class GtPlatformApplicationTests{
 
     @Autowired
