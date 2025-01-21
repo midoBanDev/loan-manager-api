b8d9560589d73f108c0b2b87b869c3ee0cb04f77
midoBanDev <amd0223@gmail.com>
2025-01-21 15:18:54 +0900

    Value import 패키지 변경.

--- src/main/java/com/gt/global/config/security/WebConfig.java
+++ src/main/java/com/gt/global/config/security/WebConfig.java
@@ -1,10 +1,10 @@
 package com.gt.global.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.google.api.client.util.Value;
 
 import lombok.extern.slf4j.Slf4j;
 
